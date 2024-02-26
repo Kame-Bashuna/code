47452909f423edf7c89841d138ad5e9ab23642c0
Kame-Bashuna <basshunakameroba@gmail.com>
2024-02-26 22:31:36 +0300

    commmented

--- index.py
+++ index.py
@@ -1,19 +1,19 @@
-Addition
+# Addition
 let x=100
 y=12
 print(x+y)
 
-Subtraction
+# Subtraction
 let x=100
 y=45
 print(x-y)
 
-Multiplication
+# Multiplication
 let x=13
 y=5
 print(x*y)
 
-Division
+# Division
 let x=100
 y=20
 print(x/y)
